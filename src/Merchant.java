f07e3a828853684258cd7f7076f926b781055a8b
Colin <vanquish.16.3.1905@gmail.com>
2026-04-23 21:48:38 +0200

    made the Merchant say how to buy products

--- src/Merchant.java
+++ src/Merchant.java
@@ -1,10 +1,20 @@
+import java.util.List;
+
 public class Merchant extends MovingActor{
     public Merchant(){
         setImage("Merchant.png");
         getImage().scale(60, 60);
 
     }
-
+    private void callPlayer(){
+        List<Player> players = getObjectsInRange(1,Player.class);
+        if(!players.isEmpty()){
+            say("Press B to see my shop");
+        }
+    }
+    public void act(){
+        callPlayer();
+    }
 
 
 }
